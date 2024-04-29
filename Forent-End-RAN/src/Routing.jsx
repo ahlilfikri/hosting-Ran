2b5e538133c647261f9b727c002b915fe5e1bddc
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-04-29 23:35:26 +0700

    fix error import

--- Forent-End-RAN/src/Routing.jsx
+++ Forent-End-RAN/src/Routing.jsx
@@ -4,16 +4,16 @@ import Produk from './LandingPage/pages/product';
 import DetailProduk from './LandingPage/pages/DetailProduk';
 import Kegiatan from './LandingPage/pages/Kegiatan';
 import DetailKegiatan from './LandingPage/pages/DetailKegiatan';
-import DashboardCms from "./CmsPage/pages/Dashboard/Dashboard";
-import KategoriCms from "./CmsPage/pages/Kategori/Kategori";
-import ProductCms from "./CmsPage/pages/Product/Product";
-import BannerCms from "./CmsPage/pages/Banner/Banner";
-import ProductPerPage from "./CmsPage/pages/Product/ProductPerPage";
-import KegitanKami from "./CmsPage/pages/KegitanKami/KegiatanKami";
-import Mitra from "./CmsPage/pages/Mitra/Mitra";
-import KegiatanPerPage from "./CmsPage/pages/KegitanKami/KegiatanPerPage";
-import Login from "./CmsPage/pages/loginregister/login"
-import Register from "./CmsPage/pages/loginregister/register"
+// import DashboardCms from "./CmsPage/pages/Dashboard/Dashboard";
+// import KategoriCms from "./CmsPage/pages/Kategori/Kategori";
+// import ProductCms from "./CmsPage/pages/Product/Product";
+// import BannerCms from "./CmsPage/pages/Banner/Banner";
+// import ProductPerPage from "./CmsPage/pages/Product/ProductPerPage";
+// import KegitanKami from "./CmsPage/pages/KegitanKami/KegiatanKami";
+// import Mitra from "./CmsPage/pages/Mitra/Mitra";
+// import KegiatanPerPage from "./CmsPage/pages/KegitanKami/KegiatanPerPage";
+// import Login from "./CmsPage/pages/loginregister/login"
+// import Register from "./CmsPage/pages/loginregister/register"
 
 
 import App from "./App";
@@ -43,46 +43,46 @@ const Routing = createBrowserRouter([
         path: "/detail-kegiatan",
         element: <DetailKegiatan />,
       },
-      {
-        path : "/dashboard",
-        element : <DashboardCms/>,
-      },
-      {
-        path : "/product/CMS",
-        element : <ProductCms/>,
-      },
-      {
-        path : "/kategori/CMS",
-        element : <KategoriCms/>,
-      },
-      {
-        path : "/banner/CMS",
-        element : <BannerCms/>,
-      },
-      {
-        path: "/product/:id",
-        element: <ProductPerPage />,
-      },
-      {
-        path: "/kegiatan/:id",
-        element: <KegiatanPerPage />,
-      },
-      {
-        path: "/kegiatan/CMS",
-        element: <KegitanKami />,
-      },
-      {
-        path: "/mitra/CMS",
-        element: <Mitra />,
-      },
-      {
-        path: "/login/cms",
-        element: <Login />,
-      },
-      {
-        path: "/register/cms",
-        element: <Register />,
-      },
+      // {
+      //   path : "/dashboard",
+      //   element : <DashboardCms/>,
+      // },
+      // {
+      //   path : "/product/CMS",
+      //   element : <ProductCms/>,
+      // },
+      // {
+      //   path : "/kategori/CMS",
+      //   element : <KategoriCms/>,
+      // },
+      // {
+      //   path : "/banner/CMS",
+      //   element : <BannerCms/>,
+      // },
+      // {
+      //   path: "/product/:id",
+      //   element: <ProductPerPage />,
+      // },
+      // {
+      //   path: "/kegiatan/:id",
+      //   element: <KegiatanPerPage />,
+      // },
+      // {
+      //   path: "/kegiatan/CMS",
+      //   element: <KegitanKami />,
+      // },
+      // {
+      //   path: "/mitra/CMS",
+      //   element: <Mitra />,
+      // },
+      // {
+      //   path: "/login/cms",
+      //   element: <Login />,
+      // },
+      // {
+      //   path: "/register/cms",
+      //   element: <Register />,
+      // },
     ],
   },
 ]);
